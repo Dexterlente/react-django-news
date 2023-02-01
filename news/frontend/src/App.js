8a4d06598cbd8e56c34577d3136f06071f4c62ab
Dexterlente <dexter.lente18@gmail.com>
2023-02-02 02:06:05 +0800

    Frontend react And Tailwind css Installed

---
+++ news/frontend/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <h1 className='text-red'>Hello World</h1>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+  Button
+</button>
+    </div>
+  );
+}
+
+export default App;
