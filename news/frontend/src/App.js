14ab7933bedc6dbbf8719bdc6988741692cc601f
Dexterlente <dexter.lente18@gmail.com>
2023-02-02 04:01:17 +0800

    configuration commented

--- news/frontend/src/App.js
+++ news/frontend/src/App.js
@@ -1,13 +1,10 @@
-
 import './App.css';
+import NavBar from './Components/NavBar'
 
 function App() {
   return (
     <div>
-        <h1 className='text-red'>Hello World</h1>
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-  Button
-</button>
+    <NavBar />
     </div>
   );
 }
---
+++ news/frontend/src/Components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+	<div>
+		<div className='bg-slate-600 h-6'>FIRST</div>
+		<div className='bg-slate-100'>NavBar</div>
+	</div>
+  )
+}
+
+export default NavBar
