499e72982fc42ec25f7c6ea32db995e96637f1bd
Dexterlente <dexter.lente18@gmail.com>
2023-02-03 05:12:14 +0800

    transition effect coverphoto mobile resposive left

--- news/frontend/src/Components/Cover.jsx
+++ news/frontend/src/Components/Cover.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 import { RxDotFilled } from 'react-icons/rx';
 
@@ -17,6 +17,11 @@ const Cover = () => {
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  const autoScroll = true;
+  let slideInterval;
+  let intervalTime = 10000;
+
+
   const prevSlide = () => {
     const isFirstSlide = currentIndex === 0;
     const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
@@ -33,31 +38,44 @@ const Cover = () => {
     setCurrentIndex(slideIndex);
   };
 
+  function auto() {
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+
+    useEffect(() => {
+      if (autoScroll) {
+        auto();
+      }
+      return () => clearInterval(slideInterval);
+    },[currentIndex]);
+
   return (
-    <div className='bg-[#FFFDD0]'>
-      <div className='max-w-[1400px] w-full m-auto py-8 h-screen px-4 relative group'>
-        <div
-          style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-          className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
-        ></div>
-        {/* Left Arrow */}
-        <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-          <BsChevronCompactLeft onClick={prevSlide} size={30} />
-        </div>
-        {/* Right Arrow */}
-        <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-          <BsChevronCompactRight onClick={nextSlide} size={30} />
-        </div>
-        <div className='flex top-4 justify-center py-2'>
-          {slides.map((slide, slideIndex) => (
-            <div
-              key={slideIndex}
-              onClick={() => goToSlide(slideIndex)}
-              className='text-2xl cursor-pointer'
-            >
-              <RxDotFilled />
-            </div>
-          ))}
+    <div>
+      <div className='bg-[#FFFDD0] h-[620px] hidden lg:block'>
+        <div className='max-w-[1280] w-11/12 m-auto py-6 h-screen px-4 relative group'>
+          <div
+            style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+            className='w-full h-4/6 rounded-2xl bg-center bg-cover  duration-500'
+          ></div>
+          {/* Left Arrow */}
+          <div className='hidden group-hover:block absolute top-[33%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+            <BsChevronCompactLeft onClick={prevSlide} size={30} />
+          </div>
+          {/* Right Arrow */}
+          <div className='hidden group-hover:block absolute top-[33%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+            <BsChevronCompactRight onClick={nextSlide} size={30} />
+          </div>
+          <div className='flex top-4 justify-center'>
+            {slides.map((slide, slideIndex) => (
+              <div
+                key={slideIndex}
+                onClick={() => goToSlide(slideIndex)}
+                className='text-2xl cursor-pointer'
+              >
+                <RxDotFilled />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
