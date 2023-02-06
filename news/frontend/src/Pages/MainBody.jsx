accb8a6db05665993a4366ecb3fa0238e74b3662
Dexterlente <dexter.lente18@gmail.com>
2023-02-06 18:01:39 +0800

    almost

--- news/frontend/src/Pages/MainBody.jsx
+++ news/frontend/src/Pages/MainBody.jsx
@@ -3,24 +3,21 @@ import car from '../Assets/car.png'
 
 const MainBody = () => {
   return (
-    <div>
-      <div className='relative flex'>
-        <div className='absolute left-9 w-2/5 '>
-            <h1>
-              TIELEREQRWQEWQEQW EVQWEQWEVW@EWQRVQWERQETRQWERQCWEQWXEQWE
+    <div className='border-b-2'>
+      <div className='flex justify-center'>
+        <div className='w-1/5'>
+            <h1 className='font-bold'>
+              TIELEREQRWQEWQEQW EVQWEQWEVW@EWQRVQWERQETRQ WERQCWEQWXEQWE
             </h1>
             <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
              when an unknown printer took a galley of type and scrambled it to make a type 
              specimen book. It has survived not only five centuries, but also the leap into 
-             electronic typesetting, remaining essentially unchanged. 
-             It was popularised in the 1960s with the release of Letraset
-              sheets containing Lorem Ipsum passages, and more recentl
-              y with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
             </p>
         </div>
-        <div className='absolute right-[200px] top-0 h-[300px] w-[440px]'>
+        <div className='h-[300px] w-[440px]'>
             <img src={ car } alt='car' />
         </div>
       </div>
