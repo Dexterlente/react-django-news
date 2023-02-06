4a289ba30bcf1cb33f214c8681042bfe3ee10290
Dexterlente <dexter.lente18@gmail.com>
2023-02-06 23:15:31 +0800

    aa

--- news/frontend/src/Components/Footer.jsx
+++ news/frontend/src/Components/Footer.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { AiOutlineCopyrightCircle } from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <div className='bg-black mt-10'>Footeraaaaaaaaaaaaawewaewaeeeeeeeeeeeeeeeeeeeeeeeeeeeaa</div>
+    <div className='bg-gray-600 p-6 justify-center flex text-white'>
+        Copyright <AiOutlineCopyrightCircle />
+         The Daily Blockchain PH 2023
+        </div>
   )
 }
 
