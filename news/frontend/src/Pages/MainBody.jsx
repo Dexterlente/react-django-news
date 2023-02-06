e5c67753f866e71a9978486f50d6fd30156c82fd
Dexterlente <dexter.lente18@gmail.com>
2023-02-06 21:17:18 +0800

    structure done for 1024

--- news/frontend/src/Pages/MainBody.jsx
+++ news/frontend/src/Pages/MainBody.jsx
@@ -3,13 +3,16 @@ import car from '../Assets/car.png'
 
 const MainBody = () => {
   return (
-    <div className='border-b-2'>
-      <div className='flex justify-center'>
-        <div className='w-1/5'>
+    <div className='border-b-2 pb-4 border-color-white mt-10'>
+      <div className='text-center font-bold text-[70px] mb-6'>
+        Top Stories
+      </div>
+      <div className='flex justify-center ml-4'>
+        <div className='w-1/5 mr-6'>
             <h1 className='font-bold'>
-              TIELEREQRWQEWQEQW EVQWEQWEVW@EWQRVQWERQETRQ WERQCWEQWXEQWE
+              TIELEREQRW QEWQEQW EVQWEQWE VW@EWQRV QWERQETRQ WERQCWE QWXEQWE
             </h1>
-            <p>
+            <p className='text-sm'>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
              when an unknown printer took a galley of type and scrambled it to make a type 
@@ -17,7 +20,22 @@ const MainBody = () => {
 
             </p>
         </div>
-        <div className='h-[300px] w-[440px]'>
+        <div className='h-[300px] w-[400px] ml-3 mr-4'>
+            <img src={ car } alt='car' />
+        </div>
+        <div className='w-1/5 mr-4'>
+            <h1 className='font-bold'>
+              TIELEREQRW QEWQEQW EVQWEQWE VW@EWQRV QWERQETRQ WERQCWE QWXEQWE
+            </h1>
+            <p className='text-sm'>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+             when an unknown printer took a galley of type and scrambled it to make a type 
+             specimen book. It has survived not only five centuries, but also the leap into 
+
+            </p>
+        </div>
+        <div className='h-[300px] w-[400px] ml-3 mr-3'>
             <img src={ car } alt='car' />
         </div>
       </div>
