177670ba7137b7560b05365bf86b6b70613ea8b9
Dexterlente <dexter.lente18@gmail.com>
2023-02-10 20:08:01 +0800

    a

--- news/frontend/src/Pages/ArticlePage.jsx
+++ news/frontend/src/Pages/ArticlePage.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import car from '../Assets/car.png'
 
 const ArticlePage = () => {
   return (
-    <div>ArticlePage</div>
+    <>
+    <div className='flex justify-center mt-10 border-b-4 mb-5'>
+      <div className='w-2/5 mr-2 ml-3'>
+      <h1 className='font-bold'>
+                TIELEREQRW QEWQEQW EVQWEQWE VW@EWQRV QWERQETRQ WERQCWE QWXEQWE
+              </h1>
+              <p className='text-sm'>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it to make a type 
+              specimen book. It has survived not only five centuries, but also the leap into 
+
+              </p>
+      </div>
+      <div className='h-[300px] w-[400px] ml-3 mr-4'>
+              <img src={ car } alt='car' />
+      </div>
+    </div>
+    <div className='flex justify-center mt-10 border-b-4 mb-5'>
+      <div className='w-2/5 mr-2 ml-3'>
+      <h1 className='font-bold'>
+                TIELEREQRW QEWQEQW EVQWEQWE VW@EWQRV QWERQETRQ WERQCWE QWXEQWE
+              </h1>
+              <p className='text-sm'>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it to make a type 
+              specimen book. It has survived not only five centuries, but also the leap into 
+
+              </p>
+      </div>
+      <div className='h-[300px] w-[400px] ml-3 mr-4'>
+              <img src={ car } alt='car' />
+      </div>
+    </div>
+    </>
   )
 }
 
