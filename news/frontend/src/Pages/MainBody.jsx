2667622bfccc26b768e5d6e645d22ec51b6bc410
Dexterlente <dexter.lente18@gmail.com>
2023-02-10 20:11:47 +0800

    a

--- news/frontend/src/Pages/MainBody.jsx
+++ news/frontend/src/Pages/MainBody.jsx
@@ -4,7 +4,7 @@ import car from '../Assets/car.png'
 
 const MainBody = () => {
   return (
-    <div className='border-b-2 pb-4 border-color-white pt-10 bg-[#FAF9F6]'>
+    <div className='border-b-4 border-color-white pt-10 mb-6 bg-[#FAF9F6]'>
       <div className='hidden lg:block'>
         <div className='text-center font-bold text-[70px] mb-6'>
           Top Stories
