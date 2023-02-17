f8afaa4eccecf947eeec0d07b4f7e6a190927883
Dexterlente <dexter.lente18@gmail.com>
2023-02-17 22:06:12 +0800

    aweawe

--- news/frontend/src/Pages/PostPage.jsx
+++ news/frontend/src/Pages/PostPage.jsx
@@ -34,27 +34,66 @@ const PostPage = () => {
                       <p>Loading...</p>
                     ) : (
                       <div>
+                        <div className='hidden sm:block mt-28'>
                           {currentPost    //filter archived false
                            .filter(post => !post.archived)
                            .map((post, index) => (
-                          <div key={post.id} className={`grid grid-cols-2 content-center border-b-2 border-[#795C34] my-10 ${
+                          <div key={post.id} className={`grid grid-cols-2 content-center pb-4 border-b-2  border-[#795C34] my-10 ${
                             index === currentPost.length - 1 ? 'border-b-0' : ''
                           }`}> 
                                 <Link to={`/post/${post.id}`}>
                                     <div className='ml-28'>
-                                      <h1 className='w-3/5 font-bold text-2xl hover:opacity-60 mr-10 content-center'>{post.title_post}
+                                      <h1 className='font-bold text-2xl hover:opacity-60 mr-2 content-center'>{post.title_post}
                                       <br/>
                                         <p className='mt-4 text-sm'>
                                             By: {post.author_post.first_name} {post.author_post.last_name}
                                         </p>
+                                        <p className='mt-4 text-sm'>
+                                        {new Date(post.time_created_post).toLocaleDateString("en-US", {
+                                                year: "numeric",
+                                                month: "long",
+                                                day: "numeric",
+                                              })}
+                                        </p>
                                       </h1>
                                     </div>
                                     </Link>
-                              <img className='h-[150px] w-[300px] border-black ml-12 mb-4' src={ post.image_post } alt='Image' />
+                              <img className='h-[150px] w-[300px]' src={ post.image_post } alt='Image' />
                           
                           </div>
                           
                           ))}
+                          </div>
+                          <div className='block sm:hidden mt-28'>
+                          {currentPost    //filter archived false
+                           .filter(post => !post.archived)
+                           .map((post, index) => (
+                          <div key={post.id} className={`flex-row border-b-2 pb-4 ml-6 content-center border-[#795C34] my-10 ${
+                            index === currentPost.length - 1 ? 'border-b-0' : ''
+                          }`}> 
+                                <Link to={`/post/${post.id}`}>
+                                    <div>
+                                      <h1 className='font-bold text-2xl hover:opacity-60 mr-10 content-center'>{post.title_post}
+                                      <br/>
+                                        <p className='mt-4 text-sm'>
+                                            By: {post.author_post.first_name} {post.author_post.last_name}
+                                        </p>
+                                        <p className='mt-4 text-sm'>
+                                        {new Date(post.time_created_post).toLocaleDateString("en-US", {
+                                                year: "numeric",
+                                                month: "long",
+                                                day: "numeric",
+                                              })}
+                                        </p>
+                                      </h1>
+                                    </div>
+                                    </Link>
+                              <img className='h-[150px] w-[300px]' src={ post.image_post } alt='Image' />
+                          </div>
+                          
+                          ))}
+
+                          </div>
                           <ReactPaginate
                             breakLabel={<span className="mr-4">...</span>}
                             pageCount={pageCount}
