c3f31ca411c3d43bd8121209718c26df2ad0aadb
Dexterlente <dexter.lente18@gmail.com>
2023-02-21 15:36:48 +0800

    Logout fix

--- news/frontend/src/Components/Logout.jsx
+++ news/frontend/src/Components/Logout.jsx
@@ -1,36 +1,3 @@
-// import React from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import Cookies from 'js-cookie';
-
-// function Logout() {
-//   const navigate = useNavigate();
-
-//   const handleLogout = () => {
-//     fetch('http://127.0.0.1:8000/api/logout/', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//         'X-CSRFToken': Cookies.get('csrftoken') // Include CSRF token
-//       },
-//     })
-//       .then(() => {
-//         Cookies.remove('sessionid'); // Remove session ID cookie
-//         navigate('/login'); // Redirect to login page
-//       })
-//       .catch((error) => {
-//         console.error('Error:', error);
-//       });
-//   };
-
-//   return (
-//     <div>
-//       <p>Are you sure you want to logout?</p>
-//       <button onClick={handleLogout}>Logout</button>
-//     </div>
-//   );
-// }
-
-// export default Logout;
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
@@ -45,6 +12,7 @@ function Logout() {
       setIsLoggedIn(true);
     } else {
       setIsLoggedIn(false);
+      navigate('/');
     }
   }, []);
 
@@ -68,7 +36,7 @@ function Logout() {
   };
 
   if (!isLoggedIn) {
-    return navigate('/login'); // or return an alternative component
+    return null; // or return an alternative component
   }
   
 
