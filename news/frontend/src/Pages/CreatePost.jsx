dceb9254a166dc32e2be242ec86b4cf7ecbe8779
Dexterlente <dexter.lente18@gmail.com>
2023-02-24 01:45:25 +0800

    a

--- news/frontend/src/Pages/CreatePost.jsx
+++ news/frontend/src/Pages/CreatePost.jsx
@@ -4,19 +4,19 @@ import Cookies from "js-cookie";
 
 const CreatePost = () => {
   const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
-  const [image, setImage] = useState("");
+  const [content, setContent] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
 
   const handleTitleChange = (event) => {
     setTitle(event.target.value);
   };
 
-  const handleBodyChange = (event) => {
-    setBody(event.target.value);
+  const handleContentChange = (event) => {
+    setContent(event.target.value);
   };
 
-  const handleImageChange = (event) => {
-    setImage(event.target.files[0]);
+  const handleImageUrlChange = (event) => {
+    setImageUrl(event.target.value);
   };
 
   const handleSubmit = (event) => {
@@ -28,17 +28,19 @@ const CreatePost = () => {
       return;
     }
 
-    const formData = new FormData();
-    formData.append("title", title);
-    formData.append("body", body);
-    formData.append("image", image);
+    const postData = {
+      title,
+      content,
+      image_url: imageUrl,
+    };
 
     const requestOptions = {
       method: "POST",
       headers: {
         Authorization: `Bearer ${sessionId}`,
+        "Content-Type": "application/json",
       },
-      body: formData,
+      body: JSON.stringify(postData),
     };
 
     fetch("http://127.0.0.1:8000/api/posts/", requestOptions)
@@ -46,8 +48,8 @@ const CreatePost = () => {
       .then((data) => {
         console.log("New post created:", data);
         setTitle("");
-        setBody("");
-        setImage("");
+        setContent("");
+        setImageUrl("");
       })
       .catch((error) => console.error(error));
   };
@@ -62,13 +64,13 @@ const CreatePost = () => {
         </label>
         <br />
         <label>
-          Body:
-          <textarea value={body} onChange={handleBodyChange} />
+          Content:
+          <textarea value={content} onChange={handleContentChange} />
         </label>
         <br />
         <label>
-          Image:
-          <input type="file" accept="image/*" onChange={handleImageChange} />
+          Image URL:
+          <input type="url" value={imageUrl} onChange={handleImageUrlChange} />
         </label>
         <br />
         <button type="submit">Create post</button>
