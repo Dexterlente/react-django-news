02608d864120c4002eeab29873fabcb85e425867
Dexterlente <dexter.lente18@gmail.com>
2023-02-25 22:26:08 +0800

    a

--- news/frontend/src/Pages/DonationPage.jsx
+++ news/frontend/src/Pages/DonationPage.jsx
@@ -6,7 +6,11 @@ const DonationPage = () => {
          Your donation can help us continue to produce high-quality content that educates and informs people about the potential of blockchain, and its impact on various industries.
          With your support, we can cover the costs of web hosting, content creation, and other essential expenses, while also expanding our coverage and reach. Whether you are a blockchain 
          enthusiast or simply believe in supporting independent journalism, your contribution can make a difference in our ability to bring reliable and unbiased news to the wider
-         community. Thank you for considering a donation to our news site, and for helping us promote transparency, innovation, and progress in the world of blockchain.</div>
+         community. Thank you for considering a donation to our news site, and for helping us promote transparency, innovation, and progress in the world of blockchain.
+         <div>
+            Gcash QR
+         </div>
+         </div>
   )
 }
 
