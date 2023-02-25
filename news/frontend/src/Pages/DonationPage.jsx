14a1d0672b1c4e774091da6fc2626b13ee003410
Dexterlente <dexter.lente18@gmail.com>
2023-02-25 19:53:51 +0800

    a

---
+++ news/frontend/src/Pages/DonationPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DonationPage = () => {
+  return (
+    <div>As a non-profit news site dedicated to providing accurate and up-to-date information about blockchain technology, we rely on the generosity of our readers to keep us running.
+         Your donation can help us continue to produce high-quality content that educates and informs people about the potential of blockchain, and its impact on various industries.
+         With your support, we can cover the costs of web hosting, content creation, and other essential expenses, while also expanding our coverage and reach. Whether you are a blockchain 
+         enthusiast or simply believe in supporting independent journalism, your contribution can make a difference in our ability to bring reliable and unbiased news to the wider
+         community. Thank you for considering a donation to our news site, and for helping us promote transparency, innovation, and progress in the world of blockchain.</div>
+  )
+}
+
+export default DonationPage
