a9ad16281620002a9ff4dda897ae57e10735c77d
Dexterlente <dexter.lente18@gmail.com>
2023-03-07 03:08:03 +0800

    donate fix

--- news/frontend/src/Pages/DonationPage.jsx
+++ news/frontend/src/Pages/DonationPage.jsx
@@ -30,8 +30,21 @@ const DonationPage = () => {
         {/* mobile device and below */}
         <div className='block md:hidden'>
            <div className='text-center'>
-             <p className='font-bold text-[34px]'> Donate To Us </p>
+             <p className='font-bold mt-8 text-[34px]'> Donate To Us </p>
             </div>
+            <div className='text-center mx-10 my-12'>
+                As a non-profit news site dedicated to providing accurate and up-to-date information about blockchain technology, we rely on the generosity of our readers to keep us running.
+                  Your donation can help us continue to produce high-quality content that educates and informs people about the potential of blockchain, and its impact on various industries.
+                  With your support, we can cover the costs of web hosting, content creation, and other essential expenses, while also expanding our coverage and reach. Whether you are a blockchain 
+                  enthusiast or simply believe in supporting independent journalism, your contribution can make a difference in our ability to bring reliable and unbiased news to the wider
+                  community. Thank you for considering a donation to our news site, and for helping us promote transparency, innovation, and progress in the world of blockchain.
+            </div>
+            <div className='content-center text-center items-center mb-12'>
+                <p className='text-[24px] mb-3'>Gcash QR</p>
+                <img src={ dexgcash } className='mx-auto h-48 w-48' alt='gcash' />
+            </div>
+
+
         </div>
     </div>
   )
