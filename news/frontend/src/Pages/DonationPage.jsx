d91918db055c12ad700c5a5f3cf7ee5b1c072b0d
Dexterlente <dexter.lente18@gmail.com>
2023-03-07 02:27:57 +0800

    a

--- news/frontend/src/Pages/DonationPage.jsx
+++ news/frontend/src/Pages/DonationPage.jsx
@@ -3,17 +3,20 @@ import dexgcash from '../Assets/dexgcash.jpg'
 
 const DonationPage = () => {
   return (
-
+  <div>
+      <div className='text-center font-bold mt-6 text-[62px]'>
+        Donate To Us
+      </div>
     <div className='grid grid-cols-2'>
-      <div className='text-center mx-8 my-12'>
-       As a non-profit news site dedicated to providing accurate and up-to-date information about blockchain technology, we rely on the generosity of our readers to keep us running.
-         Your donation can help us continue to produce high-quality content that educates and informs people about the potential of blockchain, and its impact on various industries.
-         With your support, we can cover the costs of web hosting, content creation, and other essential expenses, while also expanding our coverage and reach. Whether you are a blockchain 
-         enthusiast or simply believe in supporting independent journalism, your contribution can make a difference in our ability to bring reliable and unbiased news to the wider
-         community. Thank you for considering a donation to our news site, and for helping us promote transparency, innovation, and progress in the world of blockchain.
-         </div>
+            <div className='text-center mx-10 my-12'>
+            As a non-profit news site dedicated to providing accurate and up-to-date information about blockchain technology, we rely on the generosity of our readers to keep us running.
+              Your donation can help us continue to produce high-quality content that educates and informs people about the potential of blockchain, and its impact on various industries.
+              With your support, we can cover the costs of web hosting, content creation, and other essential expenses, while also expanding our coverage and reach. Whether you are a blockchain 
+              enthusiast or simply believe in supporting independent journalism, your contribution can make a difference in our ability to bring reliable and unbiased news to the wider
+              community. Thank you for considering a donation to our news site, and for helping us promote transparency, innovation, and progress in the world of blockchain.
+            </div>
          <div className='my-12'>
-            <div className='flex justify-center items-center font-bold text-[54px]'>
+            <div className='flex justify-center items-center text-[44px]'>
             Gcash QR
             </div>
             <img src={ dexgcash } className='mx-auto h-48 w-48' alt='gcash' />
@@ -22,6 +25,7 @@ const DonationPage = () => {
          {/* feel so tired and my gout is still here :( */}
          {/* march 1 2023 still so tired headache */}
          </div>
+    </div>
   )
 }
 
