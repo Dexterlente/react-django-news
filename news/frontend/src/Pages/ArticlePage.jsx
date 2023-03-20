1f46603d0bfe390e3a905b8966592d86343f6b8f
Dexterlente <dexter.lente18@gmail.com>
2023-03-21 05:33:29 +0800

    Pagination fixed not loading everything at onced

--- news/frontend/src/Pages/ArticlePage.jsx
+++ news/frontend/src/Pages/ArticlePage.jsx
@@ -6,29 +6,53 @@ import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 import Loading from '../Components/Loading'
 
 const ArticlePage = () => {
-  const [article, setArticle] =useState([]);
+  const [data, setData] =useState([]);
   const [loading, setLoading] = useState(true);
+  const [pageCount, setPageCount] = useState(0);
   const [currentPage, setCurrentPage] = useState(0);
 
-  const articlesPerPage = 10;
-  const pageCount = Math.ceil(article.length / articlesPerPage);
+  // const articlesPerPage = 10;
+  // const pageCount = Math.ceil(article.length / articlesPerPage);
 
-    useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/articles/")
-      .then((res) => res.json())
-      .then((data) => {
-        setArticle(data);
+  //   useEffect(() => {
+  //   fetch("http://127.0.0.1:8000/api/articles/")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setArticle(data);
+  //       setLoading(false);
+  //     });
+  // }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await fetch(`http://127.0.0.1:8000/api/articles/?page=${currentPage + 1}`);
+        const data = await result.json();
+        console.log(data);
+        setData(data.results);
+        setPageCount(Math.ceil(data.count / 10));
+        console.log(setPageCount);
         setLoading(false);
-      });
-  }, []);
+      }catch (error) {
+        console.log(error);
+        setLoading(true);
+      }    
+    };
+    fetchData();
+  }, [currentPage]);
+  // const handlePageChange = ({ selected }) => {
+  //   setCurrentPage(selected);
+  // };
 
-  const handlePageChange = ({ selected }) => {
-    setCurrentPage(selected);
-  };
-
-  const offset = currentPage * articlesPerPage;
-  const currentArticles = article.slice(offset, offset + articlesPerPage);
-
+  // const offset = currentPage * articlesPerPage;
+  // const currentArticles = article.slice(offset, offset + articlesPerPage);
+    const handlePageChange = (data) => {
+      const selectedPage = data.selected;
+      // Check if the requested page exists before making the API request
+        if (selectedPage < pageCount) {
+          console.log(pageCount);
+          setCurrentPage(data.selected);
+      }
+    };
   return (
     <div>
             {loading ? (
@@ -39,11 +63,11 @@ const ArticlePage = () => {
                           Crypto101
                         </div>
                         <div className='hidden sm:block mt-8'>
-                          {currentArticles    //filter archived false
+                          {data    //filter archived false
                           //  .filter(article => article.archived)
                            .map((article, index) => (
                           <div key={article.id} className={`grid grid-cols-2 border-b-2 pb-4 content-center border-[#795C34] my-10 ${
-                            index === currentArticles.length - 1 ? 'border-b-0' : ''
+                            index === data.length - 1 ? 'border-b-0' : ''
                           }`}> 
                                 <Link to={`/articles/${article.id}`}>
                                     <div className='ml-28'>
@@ -69,11 +93,11 @@ const ArticlePage = () => {
 
                           </div>
                           <div className='block sm:hidden mt-8'>
-                          {currentArticles    //filter archived false
+                          {data    //filter archived false
                            .filter(article => !article.archived)
                            .map((article, index) => (
                           <div key={article.id} className={`flex-row border-b-2 pb-4 ml-6 content-center border-[#795C34] my-10 ${
-                            index === currentArticles.length - 1 ? 'border-b-0' : ''
+                            index === data.length - 1 ? 'border-b-0' : ''
                           }`}> 
                                 <Link to={`/articles/${article.id}`}>
                                     <div>
