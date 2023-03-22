b17753e4aa7d61fb1affd99b8b6e9967e30d2c7f
Dexterlente <dexter.lente18@gmail.com>
2023-03-23 00:46:44 +0800

    wan

--- news/news/urls.py
+++ news/news/urls.py
@@ -22,5 +22,5 @@ from rest_framework.views import APIView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('/', TemplateView.as_view(template_name='index.html')),
 ]
