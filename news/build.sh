294902f744f77d592ce89e97193fadfad0172d94
Dexterlente <dexter.lente18@gmail.com>
2023-03-23 02:24:02 +0800

    a

--- news/build.sh
+++ news/build.sh
@@ -3,6 +3,7 @@
 set -o errexit
 
 poetry install
+npm install
 npm run build
 python manage.py collectstatic --no-input
 python manage.py migrate
--- news/news/settings.py
+++ news/news/settings.py
@@ -63,7 +63,7 @@ INSTALLED_APPS = [
     # application
     'api.apps.ApiConfig',
     'rest_framework_simplejwt',
-    'webpack_loader',
+    # 'webpack_loader',
 ]
 # import statement
 
@@ -202,11 +202,11 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 
-WEBPACK_LOADER = {
-  'DEFAULT': {
-    'CACHE': not DEBUG,
-    'STATS_FILE': os.path.join(BASE_DIR, 'frontend/webpack-stats.json'),
-    'POLL_INTERVAL': 0.1,
-    'IGNORE': [r'.+\.hot-update.js', r'.+\.map'],
-  }
-}
+# WEBPACK_LOADER = {
+#   'DEFAULT': {
+#     'CACHE': not DEBUG,
+#     'STATS_FILE': os.path.join(BASE_DIR, 'frontend/webpack-stats.json'),
+#     'POLL_INTERVAL': 0.1,
+#     'IGNORE': [r'.+\.hot-update.js', r'.+\.map'],
+#   }
+# }
