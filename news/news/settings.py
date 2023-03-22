44f0656fd6c44ac5f456798a1fa2d9bbb8136005
Dexterlente <dexter.lente18@gmail.com>
2023-03-22 17:58:42 +0800

    a

--- news/news/settings.py
+++ news/news/settings.py
@@ -153,9 +153,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'frontend/build/static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'frontend/build/static'
+# ]
+
+STATICFILES_DIRS = [os.path.join(
+    BASE_DIR, "../", "frontend", "build", "static")]
+    
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # if not DEBUG:
 #     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
