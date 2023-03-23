28d9465df837190cecba2f4aa21809f99b04dbc9
Dexterlente <dexter.lente18@gmail.com>
2023-03-24 06:51:04 +0800

    IDONTKNOW

--- news/news/urls.py
+++ news/news/urls.py
@@ -23,7 +23,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('', TemplateView.as_view(template_name='frontend/build/index.html'), name='index'),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
 
 # # Serve static files during development
